8fe2c54d0cf5fec781174412151f4ca3e5b1e2b1
Spansos <stan.spaans@student.hu.nl>
2023-09-06 16:38:58 +0200

    Update grammar.hpp

--- src/grammar.hpp
+++ src/grammar.hpp
@@ -3,31 +3,41 @@
 #include <vector>
 #include <variant>
 #include <string>
+#include <token.hpp>
 
-template < class T >
-class TerminalType
+class NonTerminalType
 {
+public:
+private:
     std::string name;
-    T match;
 };
 
 
 class NonTerminal
 {
-    std::vector< std::variant<NonTerminal, Terminal> > 
+public:
+private:
+    NonTerminalType & type;
 };
 
 
 class Rule
 {
-
+public:
+private:
+    NonTerminalType & lhs;
+    std::vector< std::variant<TokenType, NonTerminalType> > rhs;  
 };
 
 
 class Grammar
 {
     // terminals
+    std::vector<TokenType> terminals;
     // non-terminals
+    std::vector<NonTerminalType> nonTerminals;
     // rules
+    std::vector<Rule> rules;
     // start non-terminal
+    NonTerminal & start;
 };
